790351d915c968468df0f3f171b5e4801837aeb5
Qi Yang <yangqi.qiqiy@gmail.com>
2014-09-19 01:45:29 -0400

    add index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+    <head>
+       	<meta charset='utf-8'>
+       	<link type='text/css' rel="stylesheet" href="p1.css">
+       	<title>Qi Project 4</title>
+    </head>
+    <body>
+        <p>Project in Progress</p>
+     </body>
+</html>
